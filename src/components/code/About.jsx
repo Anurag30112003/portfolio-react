825e1d52e54086b331ad8c6db642b0e1abc81d9c
Anurag30112003 <anuragsharma011011@gmail.com>
2022-01-07 01:42:30 +0530

    about

--- src/components/code/About.jsx
+++ src/components/code/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import '../css/About.css'
+import profile from'../img/profile.png'
 export const About = () => {
     return (
-        <div>
-            
+        <div className='about'>
+            <h1 className='about-title'>About</h1>
+            {/* <hr className='about-hr'/> */}
+            <div className="about-text">
+                <p className='about-p'>
+                Hey there I am Anurag from India.
+                I am a full stack developer.</p>
+                <img src={profile} alt="" className='profile' />
+            </div>
         </div>
     )
 }
