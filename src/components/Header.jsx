cd02099ddf9ea37c45147b36cfd313b00814901d
Anurag30112003 <anuragsharma011011@gmail.com>
2022-01-06 18:44:43 +0530

    should-work

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import './logo'
+import './css/Header.css'
+export const Header = () => {
+    return (
+        <nav className='nav'>
+            <div className='nav-left'>
+                <a className='nav-iteam' href='/'>
+                    Home
+                </a>
+                <a className='nav-iteam' href='#about'>
+                    About
+                </a>
+                <a className='nav-iteam' href='#contact' >
+                    Contact
+                </a>
+            </div>  
+            .
+        </nav>
+    )
+}
