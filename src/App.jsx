f61a2b63670c1f4b5465b954341db59a23594c4f
Anurag30112003 <anuragsharma011011@gmail.com>
2022-01-07 14:22:19 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './App.css';
-import {Header} from './components/code/Header.jsx';
-import {Home} from './components/code/Home.jsx';
-import {About} from './components/code/About.jsx';
+import "./App.css";
+import { Header } from "./components/code/Header.jsx";
+import { Home } from "./components/code/Home.jsx";
+import { About } from "./components/code/About.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-    return (
-    <>
-    <Header />
-    <Home />
-    <About />
-    </>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Header />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 export default App;
--- src/components/code/Header.jsx
+++ src/components/code/Header.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-// import './logo'
-import '../css/Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Header.css";
 export const Header = () => {
-    return (
-        <nav className='nav'>
-                <a className='nav-item' href='/'>
-                    Home
-                </a>
-                <a className='nav-item' href='#about'>
-                    About
-                </a>
-                <a className='nav-item' href='#contact' >
-                    Contact
-                </a>
-        </nav>
-    )
-}
+  return (
+    <nav className="nav">
+      <Link className="nav-item" to="/">
+        Home
+      </Link>
+      <Link className="nav-item" to="/about">
+        About
+      </Link>
+      <Link className="nav-item" to="/contact">
+        Contact
+      </Link>
+    </nav>
+  );
+};
